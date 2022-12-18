e702e3256227a13421b1dd81d3c80e2f1964aa49
obed hipolito <obedhipolito@gmail.com>
2022-12-18 17:19:23 -0600

    integration-testing terminado Close #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EcuacionComponent } from './ecuacion/ecuacion.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EcuacionComponent
+    UiComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/arreglo/array.ts
@@ -0,0 +1,5 @@
+export function array(num1: number, num2: number, num3: number, num4: number){
+    let array1: Array<number> = [num1, num2, num3, num4];
+
+    return array1;
+}
